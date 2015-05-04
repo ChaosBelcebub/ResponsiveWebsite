0af2877cfff1d85c791128f0b3cb44cfaea1673b
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-05-04 19:01:56 +0200

    add while loop for temperature and light

--- py3/gpio.py
+++ py3/gpio.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from subprocess import call
+import grovepi
+import time 
 
 pins = [17, 18]
 
@@ -14,3 +16,40 @@ f.close()
 
 for pin in pins:
 	call(["gpio", "export", str(pin), "out"])
+
+sensor_value = 450
+sensor_value_prev = 450
+
+light_sensor = 0
+grovepi.pinMode(light_sensor, "INPUT")
+
+sensor = 1
+
+while True:
+	time.sleep(0.5)
+	try:
+		temp = grovepi.temp(sensor,'1.1')
+	
+		with open('/var/www/py3/temperature', 'w') as f:
+			f.write(str(round(temp,1)))
+		f.close()
+	except:
+		pass
+
+	try:
+		sensor_value = grovepi.analogRead(light_sensor)
+		
+	except:
+		pass
+
+	if(sensor_value < 400 and sensor_value_prev >= 400):
+		for pin in pins:
+			pset(pin, 1)
+	elif(sensor_value > 400 and sensor_value_prev <= 400):
+		for pin in pins:
+			pset(pin, 0)
+
+	sensor_value_prev = sensor_value
+
+def pset(pin, value):
+	subprocess.call(["gpio", "-g", "write", str(pin), str(value)])
