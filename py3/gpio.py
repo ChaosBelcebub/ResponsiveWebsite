5d7f35767e05d70b811ec501a9864ca134485cad
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-05-04 14:01:47 +0200

    write default value to file

--- py3/gpio.py
+++ py3/gpio.py
@@ -8,5 +8,9 @@ with open('/var/www/py3/options', 'w') as f:
 	f.write('18.0')
 f.close()
 
+with open('/var/www/py3/temperature', 'w') as f:
+	f.write('18.0')
+f.close()
+
 for pin in pins:
 	call(["gpio", "export", str(pin), "out"])
