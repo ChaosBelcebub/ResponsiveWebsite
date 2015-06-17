08bcb2b9fb719446af35373c9a1bca567f80d509
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-06-17 15:22:21 +0200

    issue fixed

--- py3/gpio.py
+++ py3/gpio.py
@@ -107,26 +107,9 @@ while True:
 				# Schreiben
 				cur = con.cursor()
 				cur.execute("insert into temp values('" + time.strftime('%d %m %Y') + "'," + time.strftime('%H') + ",'" + str(round(temp, 1)) + "')")
-				# Lesen
-				cur = con.cursor()
-				cur.execute("select * from (SELECT rowid, date, hour, t FROM temp order by rowid desc limit 48) order by rowid asc")
-
-				while True:
-					row = cur.fetchone()
-
-					if row == None:
-						break;
-
-					label.append(str(row[1]) + ' - ' + str(row[2]) + ' Uhr')
-					data.append(float(row[3]))
 		except:
 			print("Database error")
 
-		# Erstelle neues Chart
-		chart = pygal.Line(x_label_rotation=45, x_labels_major_count=12, show_minor_x_labels=True, show_legend=False, no_data_text='Keine Daten gefunden', truncate_label=25, style=LightStyle)
-		chart.title = 'Temperaturverlauf der letzten 48 Messungen'
-		chart.x_labels = label
-		chart.add('Temperatur', data)
-		chart.render_to_file('chart.svg')
-		os.rename("chart.svg", "../chart.svg")
+		# Render new Chart via external script
+		call(["python", "/var/www/py3/test.py"])
 	hour_prev = hour
